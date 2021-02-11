391bae8e015e6da4dea4eb0456ff093fb10aa7f8
Poonam-25445 <61726928+Poonam-25445@users.noreply.github.com>
2021-02-11 16:25:41 +0530

    Update CommonFiles.groovy

--- src/com/lutron/saltmarsh/CommonFiles.groovy
+++ src/com/lutron/saltmarsh/CommonFiles.groovy
@@ -1,7 +1,5 @@
 package com.lutron.saltmarsh
 
-public class CommonFiles {
    def ZipFileName(String branchName, String zipPackageType) {
       return branchName.replace("/","-")+"-$zipPackageType"+".zip"
    }
-} 
