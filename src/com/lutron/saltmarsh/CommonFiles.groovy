d61a4d9f945e87ad5adbecfbd7655cb39ad1f78a
Poonam-25445 <61726928+Poonam-25445@users.noreply.github.com>
2021-02-11 16:08:09 +0530

    Update CommonFiles.groovy

--- src/com/lutron/saltmarsh/CommonFiles.groovy
+++ src/com/lutron/saltmarsh/CommonFiles.groovy
@@ -1,12 +1,5 @@
 package com.lutron.saltmarsh
 
-class CommonFiles {
-   def ZipFileName(String branchName, String zipPackageType) {
+ def getArtifactFileName(String branchName, String zipPackageType) {
       return branchName.replace("/","-")+"-$zipPackageType"+".zip"
-   } 
-	
-   static void main(String[] args) {
-      CommonFiles ex = new CommonFiles(); 
-      println ex.ZipFileName('feature/pdf', 'artifacts')
-   } 
-}
+ }
