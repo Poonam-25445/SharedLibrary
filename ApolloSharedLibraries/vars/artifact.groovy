0822bbeb29488b8ec5fd50dd8157f21f49b04db4
Poonam-25445 <61726928+Poonam-25445@users.noreply.github.com>
2021-02-11 15:37:07 +0530

    Update artifact.groovy

--- ApolloSharedLibraries/vars/artifact.groovy
+++ ApolloSharedLibraries/vars/artifact.groovy
@@ -1,5 +1,3 @@
  def getArtifactFileName(String branchName, String zipPackageType) {
       return branchName.replace("/","-")+"-$zipPackageType"+".zip"
  } 
-	
-println getArtifactFileName('feature/pdf', 'artifacts')
