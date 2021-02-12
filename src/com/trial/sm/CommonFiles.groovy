bfc2d0ecdd3248d6a8365b204791fea3310f7085
Poonam-25445 <61726928+Poonam-25445@users.noreply.github.com>
2021-02-12 14:43:48 +0530

    Update CommonFiles.groovy

--- src/com/trial/sm/CommonFiles.groovy
+++ src/com/trial/sm/CommonFiles.groovy
@@ -1,5 +1,23 @@
-package com.trail.sm
+package com.trial.sm
 
-   def ZipFileName(String branchName, String zipPackageType) {
-      return branchName.replace("/","-")+"-$zipPackageType"+".zip"
-   }
+
+public class CommonFiles implements Artifactory {
+   
+	@Override
+   public String getArtifactFileName(Map params) {
+	   	  assert params: "params is required"
+		  assert params.branchName: "branchName is required"
+		  assert params.typeName: "zipPackageType is required"
+		  
+		  return params.branchName.replace("/","-")+"-$params.zipPackageType"+".zip323"
+	 }
+	 
+	 @Override
+	 public String getArtifactPath(Map params) {
+		 assert params: "params is required"
+		 assert params.repoPath: "repoPath is required"
+		 assert params.branchName: "zipPackageType is required"
+		 
+		 return params.repoPath + params.branchName.replace("/","%2F");
+	 }
+}
